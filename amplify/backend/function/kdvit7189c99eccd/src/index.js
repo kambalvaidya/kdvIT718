82d80da5f8970203efbd2cbed7da6e2da9b9a9be
kdv1002@unh.edu <kdv1002@unh.edu>
2023-04-24 14:39:46 -0400

    update imports

--- amplify/backend/function/kdvit7189c99eccd/src/index.js
+++ amplify/backend/function/kdvit7189c99eccd/src/index.js
@@ -26,13 +26,14 @@ exports.handler = async (event) => {
 
     return {
         statusCode: 200,
-        body: randomMovie,
+        
     
     //  Uncomment below to enable CORS requests
      headers: {
          "Access-Control-Allow-Origin": "*",
          "Access-Control-Allow-Headers": "*"
-     }
+     },
+     body: randomMovie
         
     };
 };
